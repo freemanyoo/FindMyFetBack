0c178a41c4dc010693bf9edc663ab79071001371
freemanyoo <freemanyoo@gmail.com>
2025-08-22 14:57:10 +0900

    계획서 추가 및 개인 계획서 추가 , 메인 애플리케이션 엔티티 설정 추가, user 엔티티, role Enum 생성

--- src/main/java/com/busanit501/findmyfet/FindMyFetApplication.java
+++ src/main/java/com/busanit501/findmyfet/FindMyFetApplication.java
@@ -2,7 +2,9 @@ package com.busanit501.findmyfet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 기능 사용 어노테이션
 @SpringBootApplication
 public class FindMyFetApplication {
 
