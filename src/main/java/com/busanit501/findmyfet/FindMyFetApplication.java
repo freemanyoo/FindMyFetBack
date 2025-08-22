57c0ff29cefb538893dac2d8e11b4902f1ee7c88
freemanyoo <freemanyoo@gmail.com>
2025-08-22 17:36:58 +0900

    사용자 인증 및 관리 기능 구현

--- src/main/java/com/busanit501/findmyfet/FindMyFetApplication.java
+++ src/main/java/com/busanit501/findmyfet/FindMyFetApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 기능 사용 어노테이션
 @SpringBootApplication
 public class FindMyFetApplication {
 
